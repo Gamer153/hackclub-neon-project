e2b893b16e1b53819fe439db5f4d955012011e06
Gamer153 <die.experimenter12@gmail.com>
2025-01-30 21:35:40 +0100

    Basic vue manager app setup

---
+++ manager/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main class="pa-4">
+      <MainApp />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import MainApp from './components/MainApp.vue';
+</script>
---
+++ manager/src/components/MainApp.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-form>
+    <v-card max-width="600px" v-if="!connected">
+      <v-card-title>Connect to MQTT Broker</v-card-title>
+      <v-container>
+        <v-row>
+          <v-col cols="6">
+            <v-text-field label="MQTT Broker URL">
+            </v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field label="MQTT Websocket Port">
+            </v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field label="Username">
+            </v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field label="Password">
+            </v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-card-actions>
+        <v-btn variant="tonal" @click="handleConnection">Connect</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-form>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+  const connected = ref(false);
+
+  function handleConnection() {
+    connected.value = true;
+  }
+</script>
